9eef49bff6d7afda2b03a4aa1b7240ccb3c781b9
Sundanian <thomas.krog.horne@gmail.com>
2015-03-17 11:22:31 +0100

    Har designet mission vindue

--- Guns For Hire/Guns For Hire/Form3.cs
+++ Guns For Hire/Guns For Hire/Form3.cs
@@ -17,32 +17,92 @@ namespace Guns_For_Hire
         private static SQLiteConnection dbcon = new SQLiteConnection("Data Source = current.db;Version=3");
         private static String sql = "";
         private static SQLiteCommand command = new SQLiteCommand(sql, dbcon);
-        
-        
-        
+
+
+
         public Form3()
         {
             InitializeComponent();
-
-        
-
         }
 
         private void Form3_Load(object sender, EventArgs e)
         {
-
+            dbcon.Open();
+            UpdateTables();
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
+            dbcon.Close();
             Close();
         }
 
         private void Btn_Select_Mission_Click(object sender, EventArgs e)
         {
 
-            SQLiteCommand command1 = new SQLiteCommand(sql, dbcon);
-            command1.CommandText = "select from mission where Level='" + List_Mission.SelectedItems[0].SubItems[0].Text + "'";
+            //SQLiteCommand command1 = new SQLiteCommand(sql, dbcon);
+            //command1.CommandText = "select from mission where Level='" + List_Mission.SelectedItems[0].SubItems[0].Text + "'";
+        }
+
+        private void list_Mission_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void list_Mission_Ongoing_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void list_Mission_Details_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //Nu Available_Assassins!!!
+
+        }
+        private void UpdateTables()
+        {
+            #region Clear
+            list_Mission.Clear();
+            Available_Assassins.Clear();
+            list_Mission_Ongoing.Clear();
+            list_Mission.Columns.Add("ID", 40);
+            list_Mission.Columns.Add("Lv", 40);
+            list_Mission.Columns.Add("Pay", 40);
+            list_Mission.Columns.Add("Acc", 40);
+            list_Mission.Columns.Add("Inf", 40);
+            list_Mission.Columns.Add("CK", 40);
+            list_Mission.Columns.Add("PA", 40); //Haha, PublicAss :P
+            list_Mission.Columns.Add("Primary", 60);
+            list_Mission.Columns.Add("Secondary", 70);
+            list_Mission_Ongoing.Columns.Add("ID", 40);
+            list_Mission_Ongoing.Columns.Add("Lv", 40);
+            list_Mission_Ongoing.Columns.Add("Pay", 40);
+            list_Mission_Ongoing.Columns.Add("Acc", 40);
+            list_Mission_Ongoing.Columns.Add("Inf", 40);
+            list_Mission_Ongoing.Columns.Add("CK", 40);
+            list_Mission_Ongoing.Columns.Add("PA", 40); //Haha, PublicAss :P
+            list_Mission_Ongoing.Columns.Add("Primary", 60);
+            list_Mission_Ongoing.Columns.Add("Secondary", 70);
+            Available_Assassins.Columns.Add("ID", 75);
+            Available_Assassins.Columns.Add("Name", 75);
+            Available_Assassins.Columns.Add("XP", 75);
+            Available_Assassins.Columns.Add("Level", 75);
+            #endregion
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Available_Assassins_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
--- Guns For Hire/Guns For Hire/bin/Debug/Form3.Designer.cs
+++ Guns For Hire/Guns For Hire/bin/Debug/Form3.Designer.cs
@@ -28,56 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.List_Mission = new System.Windows.Forms.ListBox();
-            this.List_Mission_detail = new System.Windows.Forms.ListBox();
-            this.List_Mission_ongoing = new System.Windows.Forms.ListBox();
             this.Btn_Select_Mission = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
+            this.list_Mission = new System.Windows.Forms.ListView();
+            this.list_Mission_Ongoing = new System.Windows.Forms.ListView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.Available_Assassins = new System.Windows.Forms.ListView();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // List_Mission
-            // 
-            this.List_Mission.FormattingEnabled = true;
-            this.List_Mission.ItemHeight = 16;
-            this.List_Mission.Location = new System.Drawing.Point(8, 124);
-            this.List_Mission.Margin = new System.Windows.Forms.Padding(2);
-            this.List_Mission.Name = "List_Mission";
-            this.List_Mission.Size = new System.Drawing.Size(419, 468);
-            this.List_Mission.TabIndex = 1;
-            // 
-            // List_Mission_detail
-            // 
-            this.List_Mission_detail.FormattingEnabled = true;
-            this.List_Mission_detail.ItemHeight = 16;
-            this.List_Mission_detail.Location = new System.Drawing.Point(429, 124);
-            this.List_Mission_detail.Margin = new System.Windows.Forms.Padding(2);
-            this.List_Mission_detail.Name = "List_Mission_detail";
-            this.List_Mission_detail.Size = new System.Drawing.Size(419, 468);
-            this.List_Mission_detail.TabIndex = 2;
-            // 
-            // List_Mission_ongoing
-            // 
-            this.List_Mission_ongoing.FormattingEnabled = true;
-            this.List_Mission_ongoing.ItemHeight = 16;
-            this.List_Mission_ongoing.Location = new System.Drawing.Point(997, 124);
-            this.List_Mission_ongoing.Margin = new System.Windows.Forms.Padding(2);
-            this.List_Mission_ongoing.Name = "List_Mission_ongoing";
-            this.List_Mission_ongoing.Size = new System.Drawing.Size(419, 468);
-            this.List_Mission_ongoing.TabIndex = 3;
             // 
             // Btn_Select_Mission
             // 
-            this.Btn_Select_Mission.Location = new System.Drawing.Point(8, 72);
-            this.Btn_Select_Mission.Margin = new System.Windows.Forms.Padding(2);
+            this.Btn_Select_Mission.Location = new System.Drawing.Point(437, 539);
+            this.Btn_Select_Mission.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Btn_Select_Mission.Name = "Btn_Select_Mission";
-            this.Btn_Select_Mission.Size = new System.Drawing.Size(417, 48);
+            this.Btn_Select_Mission.Size = new System.Drawing.Size(419, 48);
             this.Btn_Select_Mission.TabIndex = 4;
-            this.Btn_Select_Mission.Text = "Select Mission";
+            this.Btn_Select_Mission.Text = "Start Mission";
             this.Btn_Select_Mission.UseVisualStyleBackColor = true;
+            this.Btn_Select_Mission.Click += new System.EventHandler(this.Btn_Select_Mission_Click);
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(1344, 13);
+            this.button1.Location = new System.Drawing.Point(1292, 11);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 5;
@@ -85,32 +62,121 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // list_Mission
+            // 
+            this.list_Mission.Location = new System.Drawing.Point(12, 67);
+            this.list_Mission.Name = "list_Mission";
+            this.list_Mission.Size = new System.Drawing.Size(419, 468);
+            this.list_Mission.TabIndex = 6;
+            this.list_Mission.UseCompatibleStateImageBehavior = false;
+            this.list_Mission.View = System.Windows.Forms.View.Details;
+            this.list_Mission.SelectedIndexChanged += new System.EventHandler(this.list_Mission_SelectedIndexChanged);
+            // 
+            // list_Mission_Ongoing
+            // 
+            this.list_Mission_Ongoing.Location = new System.Drawing.Point(948, 67);
+            this.list_Mission_Ongoing.Name = "list_Mission_Ongoing";
+            this.list_Mission_Ongoing.Size = new System.Drawing.Size(419, 468);
+            this.list_Mission_Ongoing.TabIndex = 8;
+            this.list_Mission_Ongoing.UseCompatibleStateImageBehavior = false;
+            this.list_Mission_Ongoing.View = System.Windows.Forms.View.Details;
+            this.list_Mission_Ongoing.SelectedIndexChanged += new System.EventHandler(this.list_Mission_Ongoing_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(123, 17);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Available Missions";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(434, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(132, 17);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Available Assassins";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(945, 44);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(96, 17);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Your Missions";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 539);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(313, 17);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Remember to select a mission AND an assassin.";
+            // 
+            // Available_Assassins
+            // 
+            this.Available_Assassins.Location = new System.Drawing.Point(437, 67);
+            this.Available_Assassins.Name = "Available_Assassins";
+            this.Available_Assassins.Size = new System.Drawing.Size(419, 468);
+            this.Available_Assassins.TabIndex = 13;
+            this.Available_Assassins.UseCompatibleStateImageBehavior = false;
+            this.Available_Assassins.View = System.Windows.Forms.View.Details;
+            this.Available_Assassins.SelectedIndexChanged += new System.EventHandler(this.Available_Assassins_SelectedIndexChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(948, 540);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(419, 48);
+            this.button2.TabIndex = 14;
+            this.button2.Text = "Finnish All Missions";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // Form3
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1431, 598);
+            this.ClientSize = new System.Drawing.Size(1379, 598);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.Available_Assassins);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.list_Mission_Ongoing);
+            this.Controls.Add(this.list_Mission);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.Btn_Select_Mission);
-            this.Controls.Add(this.List_Mission_ongoing);
-            this.Controls.Add(this.List_Mission_detail);
-            this.Controls.Add(this.List_Mission);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Form3";
             this.Text = "Form3";
             this.Load += new System.EventHandler(this.Form3_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.ListBox List_Mission;
-        private System.Windows.Forms.ListBox List_Mission_detail;
-        private System.Windows.Forms.ListBox List_Mission_ongoing;
         private System.Windows.Forms.Button Btn_Select_Mission;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListView list_Mission;
+        private System.Windows.Forms.ListView list_Mission_Ongoing;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ListView Available_Assassins;
+        private System.Windows.Forms.Button button2;
 
     }
 }
